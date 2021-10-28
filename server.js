66148e7c425be44ac4a4b676629aa95d91e8e535
Peter Coyne <g00266507@gmit.ie>
2021-10-28 15:32:13 +0100

    Comments

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const path = require('path')
 const express = require('express')
 const { application } = require('express')
 const app = express()
-const port = 3000
-const bodyParser = require('body-parser')
+const port = 3000 // variable for app.listen()
+const bodyParser = require('body-parser') // require body parser middleware
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
@@ -11,16 +11,16 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-	res.send('Welcome to Data Representation & Querying')
+app.get('/', (req, res) => { // basic root route
+	res.send('Welcome to Data Representation & Querying') // send plain text response
 })
 
-app.get('/hello/:name', (req, res) => {
-	res.send(`Hello ${req.params.name}`)
+app.get('/hello/:name', (req, res) => { // route which takes a parameter "name" 
+	res.send(`Hello ${req.params.name}`)  // our paramter is available in req.params
 })
 
-app.get('/api/movies', (req, res) => {
-	const mymovies =
+app.get('/api/movies', (req, res) => { // route which will return movies in json format
+	const mymovies = // variable to store array of objects
 		[
 			{
 				"Title": "Avengers: Infinity War",
@@ -35,21 +35,21 @@ app.get('/api/movies', (req, res) => {
 				"Poster": "https://m.media-amazon.com/images/M/MV5BMjQ0MTgyNjAxMV5BMl5BanBnXkFtZTgwNjUzMDkyODE@._V1_SX300.jpg"
 			}
 		]
-	res.json({movies:mymovies})
+	res.json({movies:mymovies}) // send the response with the contents of the mymovies variable
 })
 
 app.get('/test', (req, res) => {
-	res.sendFile(__dirname + `/index.html`)
+	res.sendFile(__dirname + `/index.html`) // this uses the path package to determine the absolute path of the current directory
 })
 
 app.get('/name', (req, res) => {
-	res.send(`Hello ${req.query.fname} ${req.query.lname}`)
+	res.send(`Hello ${req.query.fname} ${req.query.lname}`)  // Response to GET request from our html form
 })
 
-app.post('/name', (req, res) => {
+app.post('/name', (req, res) => { // this is a response to a post request to /name, so the request contents won't be visible in the URL
 	res.send(`Hello ${req.body.fname} ${req.body.lname}`)
 })
 
-app.listen(port, () => {
+app.listen(port, () => { // express function to listen on port 3000 as specified in the port variable above
 	console.log(`Example app listening at http://localhost:${port}`)
 })
